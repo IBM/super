42d6daaf7bda7536b39b8818333c7050f0c8414f
Nick Mitchell <nickm@us.ibm.com>
2021-08-04 12:01:23 -0400

    chore: add cask

---
+++ Casks/super.rb
@@ -0,0 +1,27 @@
+cask "super" do
+  version "1.5.0"
+
+  name "Super"
+  desc "CLI for the Serverless Supercomputer"
+  homepage "https://github.com/IBM/super"
+
+  if Hardware::CPU.intel?
+    url "https://github.com/IBM/super/releases/download/v#{version}/Super-darwin-x64.tar.bz2"
+    sha256 "609fc0d42164f80e6216a6d2b5253fbd4af7877788c89c68018e4535c09b54ca"
+    app "Super-darwin-x64/Super.app"
+  else
+    url "https://github.com/IBM/super/releases/download/v#{version}/Super-darwin-amd64.tar.bz2"
+    sha256 "46d9c03c15222c15fc44effd06e19cc5adc9972f68e56e1f9bd30d8efedfad6c"
+    app "Super-darwin-amd64/Super.app"
+  end
+
+  livecheck do
+    url :url
+    strategy :git
+    regex(/^v(\d+(?:\.\d+)*)$/)
+  end
+
+  binary "#{appdir}/Super.app/Contents/Resources/super"
+
+  zap trash: "~/Library/Application\ Support/Super"
+end
