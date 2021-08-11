f821902f2e429e33e091f09c0e73cc470ce7414a
Nick Mitchell <nickm@us.ibm.com>
2021-08-11 14:41:37 -0400

    update Cask to 162 shasums

--- Casks/super.rb
+++ Casks/super.rb
@@ -1,5 +1,5 @@
 cask "super" do
-  version "1.6.1"
+  version "1.6.2"
 
   name "Super"
   desc "CLI for the Serverless Supercomputer"
@@ -7,11 +7,11 @@ cask "super" do
 
   if Hardware::CPU.intel?
     url "https://github.com/IBM/super/releases/download/v#{version}/Super-darwin-x64.tar.bz2"
-    sha256 "826ac658c9c919fe239994c72381189e5282dcf9725d6a350b2e7b88e30415ca"
+    sha256 "7292917f66720f01de9ce2426092956828371dadacc90b83c7deba17b5f5e32b"
     app "Super-darwin-x64/Super.app"
   else
     url "https://github.com/IBM/super/releases/download/v#{version}/Super-darwin-amd64.tar.bz2"
-    sha256 "eda14aa2e78a0c897014b41e7c82048913ccf589d77c20aadcd71d9abb8fe994"
+    sha256 "P57064bcb17c1957718a135ef5e0360a74f617d9dc274059ff438c21a49499475"
     app "Super-darwin-amd64/Super.app"
   end
 
