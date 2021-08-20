99df36376836b63fb0e2b34532eb248d0c5ad0ad
Nick Mitchell <nickm@us.ibm.com>
2021-08-20 19:44:02 -0400

    chore: bump Cask to 1.6.10

--- Casks/super.rb
+++ Casks/super.rb
@@ -1,5 +1,5 @@
 cask "super" do
-  version "1.6.9"
+  version "1.6.10"
 
   name "Super"
   desc "CLI for the Serverless Supercomputer"
@@ -7,11 +7,11 @@ cask "super" do
 
   if Hardware::CPU.intel?
     url "https://github.com/IBM/super/releases/download/v#{version}/Super-darwin-x64.tar.bz2"
-    sha256 "434ea392af01c357e8bacb9ca0246c053d12275ccfe4a06a98140350ce1b9e8a"
+    sha256 "5058159ea2de77be135b1190a05efed9ba38baff7fd8a4e771cd7447513c472a"
     app "Super-darwin-x64/Super.app"
   else
     url "https://github.com/IBM/super/releases/download/v#{version}/Super-darwin-amd64.tar.bz2"
-    sha256 "48191481feacd75ee9f1d43941f9177f9644a4792b5dec3a008513a8b53f970a"
+    sha256 "6a3d6d4e50fee0503493bdef5213df71d78083234631c12d4ecba254741a14c7"
     app "Super-darwin-amd64/Super.app"
   end
 
